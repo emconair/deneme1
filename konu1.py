bb3cd328e505be04d0ded54c67bb19658d886baa
emconair <78650492+emconair@users.noreply.github.com>
2021-02-06 17:31:29 +0300

    Create konu1.py

---
+++ konu1.py
@@ -0,0 +1,16 @@
+print('Ben emin')
+print('merhaba dünya')
+
+input('Yasiniz kac')
+input('Kilonuz kac?')
+#sep=karakterlerin arasına yerleştirilir
+#end=sadece son harfe işareti koyar
+
+print ("M","E","R","H","A","B","A",end="/",sep=".")
+
+#/n= bir alt satıra iner
+string="merhaba\n"*10
+print(string)
+
+
+
